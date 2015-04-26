b8976c1368fb94c1366c5043018628ae4c056254
Egan Anderson <p.egan.anderson@gmail.com>
2015-04-25 19:58:54 -0600

    added three functions

--- NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
+++ NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
@@ -13,14 +13,35 @@
 @end
 
 @implementation AppDelegate
+
+static int multiple = 8;
+static int divisor = 6;
             
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    // Insert code here...
+    iterateCount(4);
     
     
     return YES;
+}
+
+void iterateCount(int count){
+    if(count != 0){
+        NSLog(@"Iterate %d", count);
+        int product = multiplied(count);
+        NSLog(@"Multiplied %d by %d to be %d", count, multiple, product);
+        NSLog(@"Divided %d by %d to be %f", product, divisor, divided(product));
+        iterateCount(count-1);
+    }
+}
+
+int multiplied(int integer){
+    return integer * multiple;
+}
+
+float divided(int integer){
+    return (float)integer / divisor;
 }
 
 @end
